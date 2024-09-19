d34f1502134715ae0d0966042eed406ba28f65be
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-09-19 16:35:18 +0200

    form Create event + vue to finish later + update EventController

--- FirstTry/src/Controller/EventController.php
+++ FirstTry/src/Controller/EventController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\CreateEventFormType;
 use App\Repository\EventRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends AbstractController
 {
@@ -35,6 +38,30 @@ class EventController extends AbstractController
         
         return $this->render('event/event_info.html.twig', [
             'event' => $event,
+        ]);
+    }
+
+    // Creation Event Form
+    #[Route('/create_event', name: 'create_event')]
+    public function createEvent(Request $request): Response
+    {
+        // 1. Create new empty object
+        $event = new Event();
+        // 2. Create new Form
+        $form = $this->createForm(CreateEventFormType::class, $event);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 3.Send in DB
+            $em = $this->doctrine->getManager();
+            $em->persist($event);
+            $em->flush();
+        }
+
+        // dd($form);
+        
+        return $this->render('event/event_create_form.html.twig', [
+            'form' => $form,
         ]);
     }
 
