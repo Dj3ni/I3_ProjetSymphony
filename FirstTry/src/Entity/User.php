6f1d7b2a1d0e945055f2359bcc3662e38fb51a00
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-09-19 14:15:52 +0200

    register form + vue

--- FirstTry/src/Entity/User.php
+++ FirstTry/src/Entity/User.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use App\HydrateTrait\HydrateTrait;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use HydrateTrait;
