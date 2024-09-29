98947cf6a98b3ac62008226c7a2cd3558ac86d3b
Dj3ni <fernandezgarcia.jenny@gmail.com>
2024-09-29 17:32:51 +0200

    navstyle + submenu hidden

--- FirstTry/assets/app.js
+++ FirstTry/assets/app.js
@@ -8,3 +8,14 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Hidden submenu nav
+
+const SUB_MENU = document.getElementById("subMenu");
+const AVATAR = document.querySelector(".avatar");
+// console.log(SUB_MENU);
+// console.log(AVATAR);
+
+AVATAR.addEventListener("click", function(){
+    SUB_MENU.classList.toggle("open-menu");
+})
