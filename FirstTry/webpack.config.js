c7bc64fbf4e2d0447a50b11e9fc389c7ba19fc61
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-10-01 16:24:47 +0200

    install webpack ok , separation notok

--- FirstTry/webpack.config.js
+++ FirstTry/webpack.config.js
@@ -21,8 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry("searchForm", "./assets/searchForm.js")
-    .addEntry("hiddenSubmenu", "./assets/hiddenSubmenu.js")
+    // .addEntry("searchForm", "./assets/searchForm.js")
+    // .addEntry("hiddenSubmenu", "./assets/hiddenSubmenu.js")
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
