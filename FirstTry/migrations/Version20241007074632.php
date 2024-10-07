17e41baea368ceca92d51bef958010807b2cc02f
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-10-07 09:57:50 +0200

    delete json

--- FirstTry/migrations/Version20241007074632.php
+++ FirstTry/migrations/Version20241007074632.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241006100622 extends AbstractMigration
+final class Version20241007074632 extends AbstractMigration
 {
     public function getDescription(): string
     {
