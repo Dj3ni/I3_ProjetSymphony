901930b8ea75ef1752c33c52834a48349d1b2d63
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-10-15 12:13:49 +0200

    init google api, div for map and js(notOk)

--- FirstTry/assets/app.js
+++ FirstTry/assets/app.js
@@ -76,3 +76,23 @@ SEARCH_FORM.addEventListener("input", function(){
 //     }
 
 // })
+
+/********************* Google Map *******************/ 
+const loadGoogleMapsApi = require('load-google-maps-api');class Map {
+
+static loadGoogleMapsApi() {
+    return loadGoogleMapsApi({ key: process.env.GOOGLEMAPS_KEY });
+}  static createMap(googleMaps, mapElement) {
+    return new googleMaps.Map(mapElement, {
+    center: { lat: 45.520562, lng: -122.677438 },
+    zoom: 14
+    });
+}
+}export { Map };
+
+document.addEventListener("DOMContentLoaded", function() {
+    let mapElement = document.getElementById('map');
+    
+    Map.loadGoogleMapsApi().then(function(googleMaps) {
+    Map.createMap(googleMaps, mapElement);
+    });});
