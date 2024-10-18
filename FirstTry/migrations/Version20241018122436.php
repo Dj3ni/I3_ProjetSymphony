1e54a21cd42c711ca1aef4fd3427949863afa9ca
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-10-18 15:41:52 +0200

    avatar size change

--- FirstTry/migrations/Version20241018122436.php
+++ FirstTry/migrations/Version20241018122436.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241018121506 extends AbstractMigration
+final class Version20241018122436 extends AbstractMigration
 {
     public function getDescription(): string
     {
