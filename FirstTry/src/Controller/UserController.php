930c5977a849d1bc6afdc928b6593aa498a7dfde
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-11-05 16:32:32 +0100

    fin journée calendar occurences ok pas pour user

--- FirstTry/src/Controller/UserController.php
+++ FirstTry/src/Controller/UserController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -30,11 +32,33 @@ class UserController extends AbstractController
     }
 
     #[Route('/my_calendar', name:"user_calendar")]
-    public function personalCalendar(): Response
+    public function personalCalendar(SerializerInterface $serializerInterface): Response
     {
+        $user = $this->getUser();
+        $occurrences= $user->getOccurrencesSubscriptions();
+        $allOccurrences = [];
+        foreach ($occurrences as $occurrence){
+            $occurrence = $occurrence->getEventOccurrence();
+            $event = $occurrence->getEvent();
+            $allOccurrences[] = [
+                'id' => $event->getId(),
+                'title' => $event->getTitle(),
+                'start' => $occurrence->getDateStart()->format('Y-m-d H:i'),
+                'end' => $occurrence->getDateEnd()->format('Y-m-d H:i'),
+                'eventType' => $event->getEventType()
+            ];
+        }
+        $occurrencesJson = json_encode($allOccurrences);
+
+        // $eventsJson = $serializerInterface->serialize ($events,"json", [
+        //     AbstractNormalizer::IGNORED_ATTRIBUTES => ["eventOccurrence","user"]
+        // ]);
+        // dd($eventsJson);
+
         return $this->render("user/user_calendar.html.twig", [
             // "events"=> $events,
-
+            // "eventsJson"=>$eventsJson,
+            "occurrencesJson" => $occurrencesJson,
         ]);
     }
 
