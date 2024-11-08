75f9ea17b072109b00bd8a37cab637c01ecd24ba
Jen <fernandezgarcia.jenny@gmail.com>
2024-11-08 09:31:12 +0100

    test fixtures

--- FirstTry/src/DataFixtures/EventFixture.php
+++ FirstTry/src/DataFixtures/EventFixture.php
@@ -2,27 +2,62 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
 use App\Entity\Event;
 use App\Enum\EventType;
 use App\Entity\GamingPlace;
 use App\Enum\RecurrenceType;
 use App\DataFixtures\UserFixtures;
-use DateTime;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Filesystem\Filesystem;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class EventFixture extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create("fr_BE");
+
+        // Use Json file with content
+        $fileSystem = new Filesystem();
+        $filePath = __DIR__ .'/eventsFakeFixture.json';
+
+        if (!$fileSystem->exists($filePath)){
+            throw new FileException("Json file doesn't exists.");
+        }
+
+        $jsonData = file_get_contents($filePath);
+        $eventsData = json_decode($jsonData, true);
+
+        // Use data to do fixture
+        $dateStart = new DateTime();
+
+        foreach($eventsData as $eventData){
+            $event = new Event([
+                "title"=> $eventData["title"],
+                "dateStart" => $dateStart->modify('+' . rand(0,7) . ' months'),
+                "dateEnd" => (clone $dateStart)->modify('+' . rand(0,7) . ' days'),
+                // "dateStart"=>$eventData[new \DateTime("dateStart")]->format('d-m-Y H:i'),
+                // "dateEnd"=>$eventData[new \DateTime("dateEnd")]->format('d-m-Y H:i'),
+                "fee"=>$faker->randomFloat(2,0,100),
+                "description"=>$eventData["description"],
+            ]);
+            $organisator = $this->getReference("user0");
+            $event->setEventType(EventType::cases()[rand(0,4)]);
+            
+            $event->setRecurrenceType(RecurrenceType::cases()[rand(0,4)]);
+            $event->setUserOrganisator($organisator);
+            $manager->persist($event);
+
+            $this->addReference($eventData["title"],$event);
+        }
         
         for ($i = 0; $i < 10; $i++) {
             
             // $dateStart = $faker->dateTime();
 
-            $dateStart = new DateTime();
 
             $event = new Event([
                 "title" => $faker->catchPhrase(),
