48541b2fa2c9e24273a58123a4c1692722a79df0
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-11-12 14:33:20 +0100

    bugfix pre prod

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'user_profile')]
