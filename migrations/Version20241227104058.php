e72c9f2b7ffd7624d582ee6b14cdcdc687ad30ad
Dj3ni <fernandezgarcia.jenny@gmail.com>
2024-12-27 11:52:14 +0100

    add php unit testing bundle

--- migrations/Version20241227104058.php
+++ migrations/Version20241227104058.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241122092113 extends AbstractMigration
+final class Version20241227104058 extends AbstractMigration
 {
     public function getDescription(): string
     {
